3b225eda0579b45739f8837fc5f7bc8c8c101ba6
ngo275 <ngo38114@gmail.com>
2017-08-17 22:14:29 +0900

    initial commit

---
+++ LottieSample/LottieSample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  LottieSample-Bridging-Header.h
+//  LottieSample
+//
+//  Created by Shuichi Nagao on 2017/08/17.
+//  Copyright © 2017 Shuichi Nagao. All rights reserved.
+//
+
+#ifndef LottieSample_Bridging_Header_h
+#define LottieSample_Bridging_Header_h
+
+#import <Lottie/Lottie.h>
+
+#endif /* LottieSample_Bridging_Header_h */
--- LottieSample/ViewController.swift
+++ LottieSample/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    lazy var loadingView: LOTAnimationView = {
+        let animationView = LOTAnimationView(name: "Animation")
+        animationView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+        animationView.center = self.view.center
+        
+        animationView.loopAnimation = true
+        animationView.contentMode = .scaleAspectFit
+        animationView.animationSpeed = 1
+        
+        return animationView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func startLoading() {
+        view.addSubview(loadingView)
+        loadingView.play()
     }
-
-
+    
+    func stopLoading() {
+        loadingView.removeFromSuperview()
+    }
+    
+    func showAnimation() {
+        let animationView = LOTAnimationView(name: "Animation")
+        animationView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        animationView.center = self.view.center
+        
+        animationView.loopAnimation = false
+        animationView.contentMode = .scaleAspectFit
+        animationView.animationSpeed = 1
+        
+        view.addSubview(animationView)
+        
+        animationView.play{ finished in
+            if finished {
+                animationView.removeFromSuperview()
+            }
+        }
+    }
+    
 }
 
